b0340e27995b305d7a7b99a115f1c04af2b16a45
juanjo5516 <juanjosedelaguilalopez@gmail.com>
2020-03-25 22:28:35 -0600

    Se agregaron las relaciones (foreign key constraint) correspondientes

--- database/migrations/2014_10_12_000001_activos.php
+++ database/migrations/2014_10_12_000001_activos.php
@@ -24,23 +24,35 @@ class Activos extends Migration
             $table->integer('cantidad_producto');
             $table->string('fecha_fiscal');
             $table->string('registro_factura');
-            $table->integer('id_producto');
+            $table->bigInteger('id_producto')->unsigned();
             $table->string('comentario');
             $table->float('valor_costo',8,2);
             $table->date('fecha_ingreso');
             $table->string('codigo_sicoin');
             $table->integer('id_lugar_fisico');
-            $table->integer('id_empleado');
-            $table->integer('id_categoria');
-            $table->integer('id_estado_producto');
-            $table->integer('id_dependencia');
-            $table->integer('id_cuenta');
+            $table->bigInteger('id_empleado')->unsigned();
+            $table->bigInteger('id_categoria')->unsigned();
+            $table->bigInteger('id_estado_producto')->unsigned();
+            $table->bigInteger('id_dependencia')->unsigned();
+            $table->bigInteger('id_cuenta')->unsigned();
             $table->string('no_factura');
-            $table->integer('id_proveedor');
+            $table->bigInteger('id_proveedor')->unsigned();
             $table->float('alza',8,2);
             $table->float('baja',8,2);
             $table->timestamps();
+
+            $table->foreign('id_producto')->references('id_producto')->on('productos');
+            $table->foreign('id_estado_producto')->references('id_estadoP')->on('estado_productos');
+            $table->foreign('id_empleado')->references('id_persona')->on('personas');
+            $table->foreign('id_dependencia')->references('id_dependencia')->on('dependencias');
+            $table->foreign('id_cuenta')->references('id_cuenta')->on('cuenta_activos');
+            $table->foreign('id_proveedor')->references('id_proveedor')->on('proveedores');
+            $table->foreign('id_categoria')->references('id_categoria')->on('categoria_activos');
         });
+            
+            
+
+        
     }
 
     /**
